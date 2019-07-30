c45ce40de002fbcc8a9d159b4fb39fc25e84fdc1
c4ffein <c4ffein@gmail.com>
2019-07-30 12:59:22 +0200

    BottomTrainer.vue : new version using chart.js

--- src/nnvp-client-vue/src/components/BottomTrainer.vue
+++ src/nnvp-client-vue/src/components/BottomTrainer.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="BottomTrainer" class="BottomTrainer">
-    <div id="BottomTrainerBar" class="BottomTrainer">
+    <div id="trainer-bar" class="BottomTrainer">
       <select class="BottomTrainer bar-button" v-model="selectedDataset">
         <option v-bind:key="key" v-for="(item, key) in loadedDatasets" v-bind:value="key">
           {{key}}
@@ -10,9 +10,17 @@
         {{isTraining ? 'Stop' : 'Train'}}
       </button>
       <div class="BottomTrainer bar-button" id="experimental-warning">
-        Warning : experimental feature
+        Warning : experimental
       </div>
       <button id="button-close-trainer" v-on:click="$emit('close-trainer')">╳</button>
+    </div>
+    <div id="trainer-canvas-container">
+      <div id="trainer-canvas-container-1">
+        <canvas id="myChart1"></canvas>
+      </div>
+      <div id="trainer-canvas-container-2">
+        <canvas id="myChart2"></canvas>
+      </div>
     </div>
   </div>
 </template>
@@ -24,6 +32,8 @@ import * as tfvis from '@tensorflow/tfjs-vis';
 
 import { IMAGE_H, IMAGE_W, MnistData } from '../lib/JSDatasets/google-mnist-data';
 
+import Chart from 'chart.js';
+
 export default {
   name: 'BottomTrainer',
   data() {
@@ -33,6 +43,8 @@ export default {
       loadedDatasets: {
         'MNIST Data': '', // MnistData, empty for now...
       },
+      epochChart: null,
+      batchChart: null,
     };
   },
   methods: {
@@ -73,39 +85,76 @@ export default {
           callbacks: fitCallbacks,
         });
       }
-      // await train(model, data, i => console.log("hohoho" + i)); // ONO :(
-      async function watchTraining() {
-        const metrics = ['loss', 'val_loss', 'acc', 'val_acc'];
-        const container = {
-          name: 'show.fitCallbacks', tab: 'Training', styles: { height: '1000px' },
-        };
-        const callbacks = tfvis.show.fitCallbacks(container, metrics);
+      async function watchTraining(batchChart, epochChart) {
+        const batchMetrics = { loss: [], acc: [] };
+        const epochMetrics = { loss: [], val_loss: [], acc: [], val_acc: [] };
+        const callbacks = {
+          onBatchEnd(batchNumber, s) {
+            /*batchMetrics.loss.push(s.loss);
+            batchMetrics.acc.push(s.acc);*/
+            batchChart.data.labels.push(batchChart.data.labels.length);
+            batchChart.data.datasets[0].data.push(s.loss);
+            batchChart.data.datasets[1].data.push(s.acc);
+            batchChart.update();
+          },
+          onEpochEnd(epochNumber, s) {
+            /*epochMetrics.loss.push(s.loss);
+            epochMetrics.acc.push(s.acc);
+            epochMetrics.val_loss.push(s.loss);
+            epochMetrics.val_acc.push(s.acc);*/
+            epochChart.data.labels.push(epochChart.data.labels.length);
+            epochChart.data.datasets[0].data.push(s.loss);
+            epochChart.data.datasets[1].data.push(s.val_loss);
+            epochChart.data.datasets[2].data.push(s.acc);
+            epochChart.data.datasets[3].data.push(s.val_acc);
+            epochChart.update();
+          }
+        }
         return train(model, data, callbacks);
       }
-      await watchTraining();
+      await watchTraining(this.batchChart, this.epochChart);
     },
     switch() {
       this.isTraining = !this.isTraining;
     },
+    createChart(canvasID, chartName, initialDatasets) {
+      var ctx = document.getElementById(canvasID).getContext('2d');
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: initialDatasets,
+        },
+        options: {
+          responsive: true,
+          title: { display: true, text: chartName },
+          tooltips: { mode: 'index', intersect: false },
+          elements: { point: { radius: 0 }, line: { tension: 0 } },
+          hover: { mode: 'nearest', intersect: true },
+          scales: {
+            xAxes: [{
+              display: true, /*scaleLabel: { display: true, labelString: xLabel }*/
+            }],
+            yAxes: [{
+              display: true, /*scaleLabel: { display: true, labelString: yLabel }*/
+            }]
+          }
+        }
+      });
+      return myChart;
+    }
   },
   mounted() {
-    // You know, I love clean code. Effective, concise, beautiful.
-    // But sometimes, a prototype needs to be written on time.
-    const visorInstance = tfvis.visor();
-    if (!visorInstance.isOpen()) {
-      visorInstance.toggle();
-    }
-    visorInstance.unbindKeys();
-    const visorContainer = document.getElementById('tfjs-visor-container');
-    document.getElementById('BottomTrainer').appendChild(visorContainer);
-    visorContainer.style.height = '100%';
-    visorContainer.children[0].style.position = 'relative';
-    visorContainer.children[0].style.height = '100%';
-    visorContainer.children[0].style.width = '100%';
-    visorContainer.children[0].style.padding = 0;
-    visorContainer.children[0].children[0].style.display = 'none'; // Don't show default controls
-    visorContainer.children[0].children[1].style.display = 'none'; // Don't show default tab switch
-    visorContainer.children[0].children[2].style.padding = 0;
+    this.batchChart = this.createChart('myChart1', 'Batch', [
+      {label: 'loss', data: [], backgroundColor: 'rgba(0, 0, 255, 0.5)', color: 'rgba(0, 0, 255, 1)', borderColor: 'rgba(0, 0, 255, 0.5)', fill: false},
+      {label: 'acc', data: [], backgroundColor: 'rgba(255, 0, 0, 0.5)', color: 'rgba(255, 0, 0, 1)', borderColor: 'rgba(255, 0, 0, 0.5)', fill: false},
+    ]);
+    this.epochChart = this.createChart('myChart2', 'Epoch', [
+      {label: 'loss', data: [], backgroundColor: 'rgba(0, 0, 255, 0.5)', color: 'rgba(0, 0, 255, 1)', borderColor: 'rgba(0, 0, 255, 0.5)', fill: false},
+      {label: 'val_loss', data: [], backgroundColor: 'rgba(0, 64, 255, 0.5)', color: 'rgba(0, 64, 255, 1)', borderColor: 'rgba(0, 64, 255, 0.5)', fill: false},
+      {label: 'acc', data: [], backgroundColor: 'rgba(255, 0, 0, 0.5)', color: 'rgba(255, 0, 0, 1)', borderColor: 'rgba(255, 0, 0, 0.5)', fill: false},
+      {label: 'val_acc', data: [], backgroundColor: 'rgba(255, 64, 0, 0.5)', color: 'rgba(255, 64, 0, 1)', borderColor: 'rgba(255, 64, 0, 0.5)', fill: false},
+    ]);
   },
 };
 </script>
@@ -130,16 +179,16 @@ export default {
   flex-flow: column;
   width: 100%;
 }
-#BottomTrainerBar {
-  height: 26px;
+#trainer-bar {
+  min-height: 26px;
   overflow: hidden;
   border-bottom: 1px solid rgba(100, 100, 100, 0.3);
   background-color: rgba(100, 100, 100, 0.2);
 }
-#BottomTrainerBar > * {
+#trainer-bar > * {
   background-color: rgba(100, 100, 100, 0);
 }
-#BottomTrainerBar > *:hover {
+#trainer-bar > *:hover {
   background-color: rgba(100, 100, 100, 0.1);
 }
 .BottomTrainer.bar-button{
@@ -164,5 +213,19 @@ export default {
   border: none;
   line-height: 26px;
   transform: translate(0, -12%);
+}
+#trainer-canvas-container {
+  flex: 1;
+  padding: 20px;
+}
+#trainer-canvas-container-1{
+  width: 40%;
+  height: 800%;
+  float: left;
+}
+#trainer-canvas-container-2{
+  width: 40%;
+  height: 80%;
+  float: right;
 }
 </style>
