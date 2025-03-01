d014bf393ad18005f5580acf55f9fbaf2649f896
c4ffein <c4ffein@gmail.com>
2025-03-01 23:44:53 +0100

    breaking things

--- src/nnvp-client-vue/src/main.js
+++ src/nnvp-client-vue/src/main.js
@@ -1,21 +1,5 @@
-import Vue from 'vue';
-import App from './App.vue';
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
 
-import KerasInterface from './lib/KerasInterface/KerasInterface';
-import D3Interface from './lib/D3Interface/D3Interface';
-import jsonLayersFile from './lib/KerasInterface/generatedKerasLayers.json';
-import KeyboardListener from './lib/KeyboardListener/KeyboardListener';
-
-Vue.config.productionTip = false;
-// This file is generated from api/keras_layers.py. Temporary file for early
-// development versions, should later be automatically generated at build time.
-
-const kerasInterface = new KerasInterface(jsonLayersFile);
-Object.defineProperty(Vue.prototype, '$kerasInterface', { value: kerasInterface });
-const d3Interface = new D3Interface();
-Object.defineProperty(Vue.prototype, '$d3Interface', { value: d3Interface });
-Object.defineProperty(Vue.prototype, '$keyboardListener', { value: new KeyboardListener(d3Interface, kerasInterface) });
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app');
+createApp(App).mount('#app')
